0b89eb1394aa39fc589f74d321d8181f5171166e
Caleb Beazel <caleb.beazel@gmail.com>
2024-09-20 20:23:35 -0600

    arithmetic operations functional

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from word import Word
 from uvsim import Memory
 
-accumulator = Word()
+# accumulator = Word()
 
 memory = Memory()
 
 memory.read_file("Test1.txt")
 
-print(len(memory.memory))
-for line in memory.memory:
-    print(line.get_value())
-# word_bank = Word_bank()
-# word_bank.read_file("filename")
-
-# while word_bank.is_halted() == False:
-#     word_bank.operate()
+memory.operate()
 
--- uvsim.py
+++ uvsim.py
@@ -4,13 +4,14 @@ class Memory:
     def __init__(self):
         self.curr = 0
         self.memory = []
-        for i in range (100):
+        for _ in range (100):
             self.memory.append(Word())
+        self.accumulator = 0
+        self.is_halted = True
 
     def read_file(self,filename):
         # iterate through the lines
         with open(filename) as f:
-
             lines = f.readlines()
 
             if len(lines) > 100:
@@ -18,88 +19,97 @@ class Memory:
             
             for i, line in enumerate(lines):
                 self.memory[i].set_value(int(line))
-        # if it is an int [-9999, 9999] then store it as a word
-        # in order for the words lsit
 
-        # return 0
 
     def operate(self):
-        # start at first word and then increment by 1
+        """Resets current pointer and _is_halted. While loop iterates through each line
+        separating op_code and operand, reads in the command, and then increments self.current"""
         self.curr = 0
-        curr_word = self.memory[self.curr]
-        #find operand(last 2 digits)
-        operand = curr_word.get_value() % 100
-        #find op code(first 2 digits)
-        op_code = int(curr_word.get_value() // 100)
+        self.is_halted = False
 
-        match op_code:
-            case 10:
-                self.read(operand)
-                self.curr += 1
-            case 11:
-                self.write(operand)
-                self.curr += 1
-            case 20:
-                self.load(operand)
-                self.curr += 1
-            case 21:
-                self.store(operand)
-                self.curr += 1
-            case 30:
-                self.add(operand)
-                self.curr += 1
-            case 31:
-                self.subtract(operand)
-                self.curr += 1
-            case 32:
-                self.divide(operand)
-                self.curr += 1
-            case 33:
-                self.multiply(operand)
-                self.curr += 1
-            case 40:
-                self.branch(operand)
-                self.curr += 1
-            case 41:
-                self.branchneg(operand)
-                self.curr += 1
-            case 42:
-                self.branchzero(operand)
-                self.curr += 1
-            case 43:
-                self.halt(operand)
-                self.curr += 1
-            case _:
-                raise ValueError("Invalid op code")
-        
+        while self.is_halted == False:
+
+            curr_word = self.memory[self.curr]
+            #find operand(last 2 digits)
+            operand = curr_word.get_value() % 100
+            #find op code(first 2 digits)
+            op_code = int(curr_word.get_value() // 100)
+            # print(f"Opcode: {op_code}   Operand: {operand}")
+
+            match op_code:
+                case 10:
+                    self.read(operand)
+                    self.curr += 1
+                case 11:
+                    self.write(operand)
+                    self.curr += 1
+                case 20:
+                    self.load(operand)
+                    self.curr += 1
+                case 21:
+                    self.store(operand)
+                    self.curr += 1
+                case 30:
+                    self.add(operand)
+                    self.curr += 1
+                case 31:
+                    self.subtract(operand)
+                    self.curr += 1
+                case 32:
+                    self.divide(operand)
+                    self.curr += 1
+                case 33:
+                    self.multiply(operand)
+                    self.curr += 1
+                case 40:
+                    self.branch(operand)
+                    self.curr += 1
+                case 41:
+                    self.branchneg(operand)
+                    self.curr += 1
+                case 42:
+                    self.branchzero(operand)
+                    self.curr += 1
+                case 43:
+                    self.halt()
+                    self.curr += 1
+                case _:
+                    raise ValueError("Invalid op code")
+            
 
 
-    def is_halted(self):
-        return self.curr == -1
-    
     def read(self, operand):
-        print("read function called")
+        value = input()
+        self.memory[operand].set_value(int(value))
+        # print("read function called")
 
     def write(self, operand):
-        print("write function called")
+        print(self.memory[operand].get_value())
+        # print("write function called")
 
     def load(self, operand):
-        print("load function called")
+        self.accumulator = self.memory[operand].get_value()
+        # print("load function called")
 
     def store(self, operand):
-        print("store function called")
+        self.memory[operand].set_value(self.accumulator)
+        # print("store function called")
 
     def add(self, operand):
-        print("add function called")
+        self.accumulator += self.memory[operand].get_value()
+        # print("add function called")
 
     def subtract(self, operand):
-        print("subtract function called")
+        self.accumulator -= self.memory[operand].get_value()
+        # print("subtract function called")
 
     def divide(self, operand):
-        print("divide function called")
+        self.accumulator /= self.memory[operand].get_value()
+        # print("divide function called")
 
     def multiply(self, operand):
-        print("multiply function called")
+        self.accumulator *= self.memory[operand].get_value()
+        # print("multiply function called")
 
     def branch(self, operand):
         print("branch function called")
@@ -110,7 +120,6 @@ class Memory:
     def branchzero(self, operand):
         print("branchzero function called")
 
-    def halt(self, operand):
+    def halt(self):
+        self.is_halted = True
         print("halt function called")
-    
-    
